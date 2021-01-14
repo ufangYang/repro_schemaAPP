330278cdf74d46a10bc7d07e09acbbe6e86685a9
Yu-Fang Yang <33165978+ufangYang@users.noreply.github.com>
2021-01-14 11:17:24 +0100

    cchange the name of repo
    original: '/reproschema_template/',

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   githubSrc: 'https://raw.githubusercontent.com/user_name/repo_name/branch_name/protocols/schema_file_name.jsonld',
   banner: 'This protocol is under construction.',
   startButton: 'Start',
-  assetsPublicPath: '/reproschema_template/',
+  assetsPublicPath: '/repro_schemaAPP/',
   backendServer: 'null'
 }
